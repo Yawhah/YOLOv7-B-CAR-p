79b9572bcc5ae1ac83f87bf5fe79eaa58391c732
Yawhah <130150800+Yawhah@users.noreply.github.com>
2023-04-23 10:59:49 +0800

    self.att

--- models/common.py
+++ models/common.py
@@ -279,7 +279,7 @@ class SPPCSPC(nn.Module):
         x1 = self.cv4(self.cv3(self.cv1(x)))
         y1 = self.cv6(self.cv5(torch.cat([x1] + [m(x1) for m in self.m], 1)))
         y2 = self.cv2(x)
-        return self.cv7(torch.cat((y1, y2), dim=1))
+        return self.att(self.cv7(torch.cat((y1, y2), dim=1)))
 
 class GhostSPPCSPC(SPPCSPC):
     # CSP https://github.com/WongKinYiu/CrossStagePartialNetworks
